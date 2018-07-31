b71085a301a5e66045305e8d0a79ba3539b48ac0
yangyuliang@csu.fullerton.edu <yangyuliang@csu.fullerton.edu>
2018-07-31 09:24:06 -0700

    HW7

---
+++ chattrbox/index.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var fs = require("fs");
+var extract = require("./extract");
+var mime = require("mime");
+
+var handleError = function(err, res) {
+  var filePath = extract("/error.html");
+  console.log(filePath);
+  res.writeHead(404);
+  fs.readFile(filePath, function(req, data) {
+    res.end(data);
+  });
+};
+
+var server = http.createServer(function(req, res) {
+  console.log("Responding to a request");
+
+  var filePath = extract(req.url);
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.setHeader("Content-Type", mime.getType(filePath));
+      res.end(data);
+    }
+
+  });
+});
+
+server.listen(3000);
